7d12b60241a4911f1e144d1047360c61bb59157c
Makabaka1880 <makabaka1880@outlook.com>
2025-06-29 02:54:41 +0800

    feat: added badges to dev page

--- src/views/PageUnderDev.vue
+++ src/views/PageUnderDev.vue
@@ -6,7 +6,13 @@
         <h2>
             页面仍在开发中。
         </h2>
+
         <p>如有任何疑问，请联系<a href="mailto:makabaka1880@outlook.com">makabaka1880@outlook.com -></a></p>
+
+        <div class="padding-v-md">
+            <img src="https://github.com/makabaka1880/epitaph-frontend/actions/workflows/build.yml/badge.svg" alt="Build Vue" style="max-width: 100%; padding-right: 10px;">
+            <img src="https://github.com/makabaka1880/epitaph-frontend/actions/workflows/pages/pages-build-deployment/badge.svg" alt="Deploy Pages" style="max-width: 100%;">
+        </div>
     </div>
 </template>
 
