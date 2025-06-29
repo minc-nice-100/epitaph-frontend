6babeffa37ee1d014a372af5d13b2ffa8d63e373
Makabaka1880 <makabaka1880@outlook.com>
2025-06-30 01:45:59 +0800

    patch: updated copyright year

--- src/components/FooterNote.vue
+++ src/components/FooterNote.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="footnote">
         <div class="padding-h padding-v">
-            <span>All rights reserved © 2024 <a href="https://makabaka1880.xyz">李璟之子 @Makabaka1880</a>. </span> <br/><br/>
+            <span>All rights reserved © 2025 <a href="https://makabaka1880.xyz">李璟之子 @Makabaka1880</a>. </span> <br/><br/>
             <span>特别感谢<a href="https://github.com/abloom25">前端开发 @ABloom</a> 与<a href="https://github.com/minc-nice-100">运营部署 @minc_nice_100</a>.</span><br/><br/>
             <span>愿逝者安息.</span>
         </div>
