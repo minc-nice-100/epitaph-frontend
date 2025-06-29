6097c2a1cf71e13ceef2000140dd66414198065a
Makabaka1880 <makabaka1880@outlook.com>
2025-06-29 15:27:59 +0800

    Added grayscale

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import './style.css'
 import { apiBaseUrl } from '@/constants.mjs'
-fetch(`${apiBaseUrl}/grayscale/is-today`)
+fetch(`${apiBaseUrl}/grayscale/today`)
     .then(res => res.json())
     .then(res => {
-        if (res.today) {
+        if (res.gray) {
             const style = document.createElement('style');
             style.textContent = `html { filter: grayscale(100%); }`;
             document.head.appendChild(style);
