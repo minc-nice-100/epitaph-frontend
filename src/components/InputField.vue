b24511fea6ddd5f5af4e54aa023dc16cb6f8559e
Abloom | 唤青 <evereen2023@gmail.com>
2025-06-30 01:32:10 +0800

    fixbug

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -1,26 +1,22 @@
-<!-- InputField.vue -->
 <template>
-	<p>{{ ques }}</p>
-	<input
-		v-model="updateValue"
-	/>
+  <p>{{ ques }}</p>
+  <input
+    v-model="updateValue"
+  />
 </template>
 
 <script setup>
-import {watch, toRef, computed } from 'vue';
+import { computed } from 'vue';
 
-const props = defineProps(['modelValue']);
+const props = defineProps({
+  modelValue: String,
+  ques: String
+});
+
 const emit = defineEmits(['update:modelValue']);
 
 const updateValue = computed({
-	get: () => props.modelValue, 
-	set: (val) => emit('update:modelValue', val)
+  get: () => props.modelValue,
+  set: (val) => emit('update:modelValue', val)
 });
-
-const modelValueRef = toRef(props, 'modelValue');
-
-watch(modelValueRef, (newVal) => {
-	console.log('Input updated:', newVal);
-});
-
 </script>
